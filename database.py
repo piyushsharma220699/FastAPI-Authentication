fcd64cd0466058ca18940d05fc74d705e2ec2d93
Piyush Sharma <piyush.sharma@kotak.com>
2023-08-18 00:17:58 +0530

    Added OTP Verification

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 import sqlite3
+from dotenv import load_dotenv
+import os
 
-connection = sqlite3.connect('./sqlite.db')
+load_dotenv()
+DB_LOCATION=os.getenv("DB_LOCATION")
+
+
+connection = sqlite3.connect(str(DB_LOCATION))
 cursor = connection.cursor()
 
 # Create a user_login_data in SQLite
@@ -10,7 +16,11 @@ CREATE TABLE user_login_data (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(50) UNIQUE,
     salt TEXT,
-    password_hash TEXT
+    password_hash TEXT,
+    email VARCHAR(50) UNIQUE,
+    otp_generated INTEGER,
+    otp_generation_time INTEGER,
+    verification_status INTEGER
 )
 '''
 )
--- main.py
+++ main.py
@@ -1,38 +1,61 @@
 from fastapi import FastAPI, Body, HTTPException
 import hashlib
 from dotenv import load_dotenv
+from email.message import EmailMessage
 import os
-import sqlite3
+import random
 import secrets
+import smtplib
+import sqlite3
+import time
 
 load_dotenv()
 
 DB_LOCATION=os.getenv("DB_LOCATION")
+VERIFICATION_EMAIL_ID=os.getenv("VERIFICATION_EMAIL_ID")
+VERIFICATION_PASSWORD=os.getenv("VERIFICATION_PASSWORD")
 
 app = FastAPI()
 
+def send_otp_to_user(email: str, otp: int):
+    message = EmailMessage()
+    message.set_content(f"Your OTP Code is: {otp}")
+    message["Subject"] = "TerraVision OTP Verification"
+    message["From"] = VERIFICATION_EMAIL_ID
+    message["To"] = email
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(str(VERIFICATION_EMAIL_ID),str(VERIFICATION_PASSWORD))
+        server.send_message(message)
+        
 @app.post("/api/v1/register")
-def register_user(username: str = Body(), password: str = Body()):
+def register_user(username: str = Body(), password: str = Body(), email: str = Body()):
     salt = str(secrets.token_bytes(16).hex())
     password_with_salt = password + salt
 
     hashed_password = hashlib.sha256(password_with_salt.encode()).hexdigest()
+
+    otp_generated = random.randint(100000,999999)
+    otp_generation_time = int(time.time())
 
     try:
         connection = sqlite3.connect(database=str(DB_LOCATION))
         cursor = connection.cursor()
 
         cursor.execute('''
-            INSERT INTO user_login_data (username, salt, password_hash)
-            VALUES (?,?,?)
-        ''', (username,salt,hashed_password))
+            INSERT INTO user_login_data (username, salt, password_hash, email, otp_generated, otp_generation_time, verification_status)
+            VALUES (?,?,?,?,?,?,?)
+        ''', (username,salt,hashed_password,email,otp_generated,otp_generation_time,1))
         
         connection.commit()
         connection.close()
     except Exception as e:
         raise HTTPException(status_code=400 ,detail=str(e))
 
-    return {"user_id" : username}
+    send_otp_to_user(email,otp_generated)
+
+    return {"user_id" : username, "message" : "User Successfully Registered. You have received a mail on the above E-Mail ID. Please verify your identity to move forward."}
 
 @app.get('/api/v1/login')
 def login_user(username: str, password: str):
